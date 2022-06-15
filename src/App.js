cb24b5815287d1b050381ee31f8837454b97ca39
Chalermkiat Tiawpanichkij <extreme.gamie@gmail.com>
2022-06-15 21:05:19 +0700

    FIN todo app on React Frameworkl
    React + JS + Bootstrap + CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button, button, Card, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
+
+function Todo({ todo, index, markTodo, removeTodo }) {
+  return (
+    <div className="todo">
+      <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>
+        {todo.text}
+      </span>
+      <div>
+        <Button variant="outline-success" onClick={() => markTodo(index)}>
+          Done
+        </Button>
+        <Button variant="outline-danger" onClick={() => removeTodo(index)}>
+          Delete
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+function FormTodo({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <Form.Group>
+          <Form.Label>
+            <b>Add Todo</b>
+          </Form.Label>
+          <Form.Control
+            type="text"
+            className="input"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="Add new todo E.G. Read Book"
+          />
+        </Form.Group>
+      </div>
+      <Button variant="primary mb-3" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    {
+      text: "This is an example todo",
+      isDone: false,
+    },
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todoList, { text }];
+    setTodoList(newTodos);
+  };
+
+  const markTodo = (index) => {
+    const newTodos = [...todoList];
+    newTodos[index].isDone = true;
+    setTodoList(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todoList];
+    newTodos.splice(index, 1);
+    setTodoList(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h1 className="text-center mb-4">Todo List</h1>
+        <FormTodo addTodo={addTodo} />
+        <div>
+          {todoList.map((todo, index) => (
+            <Card>
+              <Card.Body>
+                <Todo
+                  key={index}
+                  index={index}
+                  todo={todo}
+                  markTodo={markTodo}
+                  removeTodo={removeTodo}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
